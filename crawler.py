dbaf4e9486cb1720ab3c76b432b031f3b477bac2
wangyongjian <yongjianw_92@163.com>
2018-04-12 17:58:46 +0800

    add my crawler.py

---
+++ crawler.py
@@ -0,0 +1,30 @@
+import urllib2
+import re
+
+req = urllib2.Request('http://www.budejie.com/text/2')
+req.add_header('User-Agent','Mozilla/5.0 (X11;Ubuntu;Linux x86_64;rv:31.0) Gecko/20100101 Firefox/31.0')
+req.add_header('Referer','http://www.budejie.com/text/1')
+resp = urllib2.urlopen(req)
+
+#print resp.geturl()
+#print resp.getcode()
+
+html = resp.read().decode('utf-8')
+
+#print html
+
+pattern = re.compile(r'<div class="j-r-list-c-desc">\s+(.*)\s+</div>')
+subpattern = re.compile(r'<.*?>')
+result = re.findall(pattern,html)
+print '--------------before process:'
+print result[0]
+print '--------------after process:'
+
+process_result = []
+for passage in result:
+    process_result.append(re.sub(subpattern,'',passage))
+
+for p_passage in process_result:
+    print '================' * 5
+    print p_passage
+
